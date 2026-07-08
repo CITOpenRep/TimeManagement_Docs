ff742806c9f1a9181ce1276d73dc2c63fadbe332
Parvathy Nair <parvathyabnair0203@gmail.com>
2026-07-08 15:49:36 +0530

    [ADDED] app-translation-guide for the contributors under contribution section,[ADDED] the dutch version of the corresponding file to the i18n/nl folder

--- sidebars.js
+++ sidebars.js
@@ -67,7 +67,8 @@ const sidebars = {
       items: [
         "contributing/getting-started",
         "contributing/pull-request-guidelines",
-        "contributing/documentation-governance"
+        "contributing/documentation-governance",
+        "contributing/app-translation-guide"
       ]
     }
   ]
